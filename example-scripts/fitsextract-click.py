cf4190930097f79641295d370f8822e42b970baa
Geert Barentsen <geert@barentsen.be>
2016-06-20 14:15:11 +0100

    Makes the argument names consistent

--- example-scripts/fitsextract-click.py
+++ example-scripts/fitsextract-click.py
@@ -7,16 +7,16 @@ def write_fits_extension(input_fn, extension, output_fn, clobber):
 
 
 @click.command()
-@click.argument('input_fn', type=click.File('rb'))
+@click.argument('filename', type=click.File('rb'))
 @click.argument('extension', type=int)
-@click.option('--output_fn', help='Output filename.')
+@click.option('--output', help='Output filename.')
 @click.option('--clobber', is_flag=True, envvar='CLOBBER',
               help='Allow the output file to be overwritten')
-def fitsextract_main(input_fn, extension, output_fn, clobber):
-    if output_fn is None:
-        output_fn = '{}-ext{}.fits'.format(input_fn.name, extension)
-    click.echo('Writing {}'.format(output_fn))
-    write_fits_extension(input_fn, extension, output_fn, clobber)
+def fitsextract_main(filename, extension, output, clobber):
+    if output is None:
+        output = '{}-ext{}.fits'.format(filename.name, extension)
+    click.echo('Writing {}'.format(output))
+    write_fits_extension(filename, extension, output, clobber)
 
 
 if __name__ == '__main__':
